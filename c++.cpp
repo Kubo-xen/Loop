755267679e72ca3f13e181431a8370c8432d24e4
Kubo-xen <abriansahd@gmail.com>
2026-02-08 18:59:57 +0700

    Loop With C++

---
+++ c++.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // with i++
+    for (int i=1; i<=10; i++) {
+        cout << i << " ";
+    }
+
+    cout << endl;
+    
+    // with i--
+    for (int i=10; i>0; i--) {
+        cout << i << " ";
+    }
+    
+    cout << endl;
+
+    return 0;
+}
